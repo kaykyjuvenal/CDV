1490af8f5a872a3e5ab474e16477880cf325641c
kaykyjuvenal <neguito.juvenal@gmail.com>
2024-08-26 21:37:10 -0300

    Inicio do desenvolvimento
    1º Adição ao código

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/cdv/MainActivity.kt
@@ -0,0 +1,26 @@
+package br.edu.ifsp.scl.ads.cdv
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.ads.cdv.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(amb.root)
+        Log.v(getString(R.string.app_name),"onCreate: Iniciando ciclo completo")
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.v(getString(R.string.app_name),"onStart: Iniciando ciclo de vida")
+    }
+
+
+}
